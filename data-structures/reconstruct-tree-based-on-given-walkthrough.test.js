09dde232b0df7e705d7b31af0faf4e3f8e3a0d76
Dusan Trtica <dusan.trtica@hotmail.com>
2024-06-03 15:57:46 +0200

    unfinished reconstruction

---
+++ data-structures/reconstruct-tree-based-on-given-walkthrough.test.js
@@ -0,0 +1,27 @@
+const postfixOrderFrom = (prefix = '', infix = '') => {
+  if (prefix === '') {
+    return ''
+  }
+  const root = prefix[0]
+  const leftSubtreeLength = infix.indexOf(root)
+
+  const leftSubtreePrefix = prefix.substring(1, leftSubtreeLength)
+  const leftSubtreeInfix = infix.substring(0, leftSubtreeLength)
+
+  const rightSubtreePrefix = prefix.substring(1 + leftSubtreeLength)
+  const rightSubtreeInfix = infix.substring(1 + leftSubtreeLength)
+
+  return (
+    postfixOrderFrom(leftSubtreePrefix, leftSubtreeInfix) +
+    postfixOrderFrom(rightSubtreePrefix, rightSubtreeInfix) +
+    root
+  )
+}
+
+describe('suffix order from prefix and infix', () => {
+  it('returns suffix order when prefix and infix were given', () => {
+    const prefix = 'abecfg' // Root, Left, Right
+    const infix = 'beafcg' // Left, Root, Right
+    expect(postfixOrderFrom(prefix, infix)).toEqual('ebfgca')
+  })
+})
